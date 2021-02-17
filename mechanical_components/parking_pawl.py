0c7bb201749a06d5aba3fe346009e59f52701a52
Steven Masfaraud <masfaraud@dessia.tech>
2021-02-17 13:31:04 +0100

    Separating parking pawl

--- mechanical_components/parking_pawl.py
+++ mechanical_components/parking_pawl.py
@@ -218,31 +218,3 @@ class ParkingPawl(dc.DessiaObject):
                                          action_line.plot_data(edge_style=red_dashed)]
                                          +self.pawl.plot_data()[0].primitives)]
 
-# wheel = Wheel(0.020, 0.080, 0.060, 9, 0.25, 0.3, 0.035)
-# wheel.outer_contour().plot()
-# wheel.babylonjs()
-# pawl = Pawl(vm.Point2D(-0.06, 0.042), 0.03, 0.025, 0.030,finger_height=0.012,
-#             finger_angle=math.radians(20), finger_width=0.008, slope_height_start=0.015,
-#             slope_angle=math.radians(12), slope_offset=0.005, slope_length=0.035,
-#             width=0.030)
-
-# parking_pawl = ParkingPawl(wheel, pawl)
-parking_pawl = ParkingPawl(0.030, 0.060, 0.080,
-                           9, 0.25, 0.3,
-                           basis_diameter=0.030,
-                           contact_diameter=0.070,
-                           width = 0.035,
-                           pawl_offset = 0.06,
-                           axis_inner_diameter=0.025,
-                           axis_outer_diameter=0.030,
-                           finger_height=0.012,
-                           finger_angle=math.radians(20), finger_width=0.008,
-                           slope_start_height=0.015,
-                           slope_angle=math.radians(12),
-                           slope_offset=0.005, slope_length=0.035)
-# parking_pawl.pawl.outer_contour().plot()
-# parking_pawl.wheel.outer_contour().plot()
-# plot_data.plot_canvas(plot_data_object=parking_pawl.wheel.plot_data()[0], debug_mode=True)
-# parking_pawl.babylonjs()
-parking_pawl.mpl_plot()
-print(parking_pawl.check())
