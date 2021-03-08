e108c4c2878ef4ae1da32b05c08b34bc999ae71d
Steven Masfaraud <masfaraud@dessia.tech>
2021-03-08 23:32:26 +0100

    Chains begin

---
+++ mechanical_components/chains.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+from typing import List
+import dessia_common as dc
+import volmdlr as vm
+import volmdlr.edges as vme
+import volmdlr.wires as vmw
+import volmdlr.primitives3d as p3d
+
+
+class RollerChain(dc.DessiaObject):
+    def __init__(self, pitch:float, pin_diameter, roller_outer_diameter, inner_width, overall_width):
+        dc.DessiaObject.__init__(self, pitch=pitch,
+                                 pin_diameter=pin_diameter,
+                                 roller_outer_diameter=roller_outer_diameter,
+                                 inner_width=inner_width,
+                                 overall_width=overall_width)
+        self.bushing_diameter = 0.5*(self.roller_outer_diameter+self.pin_diameter)
+        self.plate_width = 0.35*(self.overall_width - self.inner_width)
+        self.slack = 0.15*(self.overall_width - self.inner_width)
+        self.plate_diameter = 1.2*self.roller_outer_diameter
+        
+    def plate_outer_contour(self):
+        circle1 = vmw.Circle2D(vm.O2D, 0.5*self.pin_diameter),
+                
+
+    def plate_inner_contours(self):
+        return [vmw.Circle2D(vm.O2D, 0.5*self.pin_diameter),
+                vmw.Circle2D(vm.X2D*self.pitch, 0.5*self.pin_diameter)]
+
+        
+    def volmdlr_primitives(self, frame=vm.OXYZ):
+        pin1 = p3d.Cylinder(frame.origin, frame.u, 0.5*self.pin_diameter, self.overall_width)
+        pin2 = p3d.Cylinder(frame.origin+self.pitch*vm.Y3D, frame.u,
+                            0.5*self.pin_diameter, self.overall_width)
+        outer_plate1 = p3d.ExtrudedProfile(frame.origin, frame.v, frame.w,
+                                           self.plate_outer_contour,
+                                           self.plate_inner_contours,
+                                           vm.X3D)
+        return [pin1, pin2, outer_plate1]#, outer_plate2, inner_plate1, inner_plate2]
+
+class Sprocket(dc.DessiaObject):
+    def _init__(self, pitch:float, number_teeth:int):
+        dc.DessiaObject.__init__(self, pitch=pitch, number_teeth=number_teeth)    
+    
+    
+class RollerChainLayout(dc.DessiaObject):
+    def __init__(self, roller_chain:RollerChain, sprockets:List[Sprocket]):
+        dc.DessiaObject.__init__(self, roller_chain=roller_chain,
+                                 sprockets=sprockets)
+    
+# https://www.tridistribution.fr/chaines-a-rouleaux-norme-europeene-iso/6184-5453-rollerchain-simplex-european-series.html#/4017-ref-04b1/4018-p-6/17601-w-280/17602-o_d-400/17603-o_d-185/17604-c-830
+iso_chains = [RollerChain(pitch=0.006, inner_width=0.002,
+                          overall_width=0.0083, roller_outer_diameter=0.004,
+                          pin_diameter=0.00185)]
+        
+iso_chains[0].babylonjs()
+        
