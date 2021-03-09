64f4878b05557868f513dbc3945871f3eece8be0
Steven Masfaraud <masfaraud@dessia.tech>
2021-03-09 15:42:15 +0100

    wip: plate profiles

--- mechanical_components/chains.py
+++ mechanical_components/chains.py
@@ -25,9 +25,30 @@ class RollerChain(dc.DessiaObject):
         self.plate_diameter = 1.2*self.roller_outer_diameter
         
     def plate_outer_contour(self):
-        circle1 = vmw.Circle2D(vm.O2D, 0.5*self.pin_diameter),
-                
-
+        circle1 = vmw.Circle2D(vm.O2D, 0.5*self.plate_diameter)
+        circle2 = vmw.Circle2D(self.pitch*vm.X2D, 0.5*self.plate_diameter)
+        circle3 = vmw.Circle2D(vm.Point2D(0.5*self.pitch, 2.5*self.plate_diameter), 2.1*self.plate_diameter)
+        circle4 = vmw.Circle2D(vm.Point2D(0.5*self.pitch, -2.5*self.plate_diameter), 2.1*self.plate_diameter)
+        
+        p1 =  sorted(circle1.circle_intersections(circle3), key=lambda p:p.x)[1]
+        p2 =  sorted(circle2.circle_intersections(circle3), key=lambda p:p.x)[0]
+        p3 =  sorted(circle2.circle_intersections(circle4), key=lambda p:p.x)[0]
+        p4 =  sorted(circle1.circle_intersections(circle4), key=lambda p:p.x)[1]
+        # ax = circle1.plot()
+        # circle2.plot(ax=ax)        
+        # circle3.plot(ax=ax)        
+        # circle4.plot(ax=ax)        
+        # p1.plot(ax=ax, color='r')
+        # p2.plot(ax=ax, color='g')
+        # p3.plot(ax=ax, color='b')
+        # p4.plot(ax=ax)
+        _, arc1  = circle1.split(p4, p1)
+        arc2, _ = circle3.split(p1, p2)
+        _, arc3 = circle2.split(p2, p3)
+        _, arc4 = circle4.split(p3, p4)
+        # arc4.plot(ax=ax, color='g')
+        return vmw.Contour2D([arc1, arc2, arc3, arc4])
+        
     def plate_inner_contours(self):
         return [vmw.Circle2D(vm.O2D, 0.5*self.pin_diameter),
                 vmw.Circle2D(vm.X2D*self.pitch, 0.5*self.pin_diameter)]
@@ -38,8 +59,8 @@ class RollerChain(dc.DessiaObject):
         pin2 = p3d.Cylinder(frame.origin+self.pitch*vm.Y3D, frame.u,
                             0.5*self.pin_diameter, self.overall_width)
         outer_plate1 = p3d.ExtrudedProfile(frame.origin, frame.v, frame.w,
-                                           self.plate_outer_contour,
-                                           self.plate_inner_contours,
+                                           self.plate_outer_contour(),
+                                           self.plate_inner_contours(),
                                            vm.X3D)
         return [pin1, pin2, outer_plate1]#, outer_plate2, inner_plate1, inner_plate2]
 
@@ -58,5 +79,5 @@ iso_chains = [RollerChain(pitch=0.006, inner_width=0.002,
                           overall_width=0.0083, roller_outer_diameter=0.004,
                           pin_diameter=0.00185)]
         
-iso_chains[0].babylonjs()
+iso_chains[0].plate_outer_contour()
         
